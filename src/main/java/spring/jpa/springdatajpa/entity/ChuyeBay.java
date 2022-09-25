6b62f0c8aa12e4bf9ddfc505f1a6e607d485ab95
Your Name <you@example.com>
2022-09-25 13:25:37 +0700

    add maybay

--- src/main/java/spring/jpa/springdatajpa/entity/ChuyeBay.java
+++ src/main/java/spring/jpa/springdatajpa/entity/ChuyeBay.java
@@ -2,6 +2,7 @@ package spring.jpa.springdatajpa.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
